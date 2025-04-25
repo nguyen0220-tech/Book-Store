c896c5bd31c5a41d513c3b3fddf07a1784fa1c6c
nguyen0220 <teemee202@gmail.com>
2025-04-25 12:01:48 +0900

    sua lai code

--- src/main/java/catholic/ac/kr/secureuserapp/model/dto/UserDTO.java
+++ src/main/java/catholic/ac/kr/secureuserapp/model/dto/UserDTO.java
@@ -2,11 +2,6 @@ package catholic.ac.kr.secureuserapp.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-<<<<<<< HEAD
-import lombok.NoArgsConstructor;
-import lombok.Value;
-=======
->>>>>>> 7b5a3ea9576dda0e2ba2152a997e3960ebd1e7c8
 
 @Data
 public class UserDTO {
