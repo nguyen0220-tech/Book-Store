e919a087d0be2f730f8599820e8fefcc84be0adf
nguyen0220 <teemee202@gmail.com>
2025-05-16 13:53:34 +0900

    edit logger

--- src/main/java/catholic/ac/kr/secureuserapp/Aspect/LoggingAspect.java
+++ src/main/java/catholic/ac/kr/secureuserapp/Aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package catholic.ac.kr.secureuserapp.Aspect;
 
+import catholic.ac.kr.secureuserapp.model.dto.SignupRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -13,20 +14,26 @@ public class LoggingAspect {
 
     // Advice này sẽ được chạy "xung quanh" tất cả các method trong gói service
     // Expression AOP để chỉ định các method cần áp dụng: * là tất cả kiểu trả về, (..) là mọi tham số
-    @Around("execution(* catholic.ac.kr.secureuserapp.service.*.*(..)),")
+    @Around("execution(* catholic.ac.kr.secureuserapp.service.*.*(..)) ||" +
+            "execution(* catholic.ac.kr.secureuserapp.controller.*.*(..))")
     public Object logMethodExecution(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodName = joinPoint.getSignature().toLongString();
-        log.info("==> Bắt đầu gọi: " + methodName);
+        log.info("==> Bắt đầu gọi: {}", methodName);
+
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof SignupRequest signupRequest) {
+                log.info("==> SignupRequest: {}", signupRequest.getUsername());
+            }
+        }
 
         long startTime = System.currentTimeMillis();
 
         Object result = joinPoint.proceed();
 
         long duration = System.currentTimeMillis() - startTime;
-        log.info("<== Kết thúc: " + methodName + " | Thời gian: " + duration + "ms");
+        log.info("<== Kết thúc: {} | Thời gian: {}ms", methodName, duration);
 
         return result;
     }
-
-
 }
