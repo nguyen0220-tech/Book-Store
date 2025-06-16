7112d3df450a3cccf97202ffb4fec8dbdd97292b
nguyen0220 <teemee202@gmail.com>
2025-06-16 11:42:00 +0900

    Initial commit for Book Store project

--- src/main/java/catholic/ac/kr/secureuserapp/BookApplication.java
+++ src/main/java/catholic/ac/kr/secureuserapp/BookApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = "catholic.ac.kr.secureuserapp")
 @EnableCaching // bật để test Cache: spring sẽ luư cache vao RAM (thực tế sẽ cần cài theem phần mềm nhu Redis, Doker...)
-public class UserApplication {
+public class BookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(BookApplication.class, args);
     }
 
 }
